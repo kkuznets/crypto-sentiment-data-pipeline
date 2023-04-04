890874ff10fa515dcb4d4b91ba7894300450118e
Kirill Kuznetsov <kirill@kkuznets.dev>
2023-04-04 13:52:09 +1000

    replace test flow

--- flows/test_flow.py
+++ flows/test_flow.py
@@ -3,7 +3,7 @@ from platform import node, platform
 
 
 @flow
-def hello(user_input: str = "World"):
+def test_flow(user_input: str = "World"):
     logger = get_run_logger()
     logger.info("Hello from Prefect, %s! 🚀", user_input)
     logger.info("Network: %s. Instance: %s. Agent is healthy ✅️", node(), platform())
