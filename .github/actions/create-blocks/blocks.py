c731ada354429852fc1d0e41181a4587f3f32a2f
Kirill Kuznetsov <kirill@kkuznets.dev>
2023-04-06 13:16:09 +1000

    fix workflows and block names

--- .github/actions/create-blocks/blocks.py
+++ .github/actions/create-blocks/blocks.py
@@ -7,10 +7,9 @@ from prefect.filesystems import GitHub
 from prefect_gcp.cloud_run import CloudRunJob
 from prefect_gcp.credentials import GcpCredentials
 
-REPO = "https://github.com/anna-geller/prefect-cloud-gcp"
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--branch", default="main")
-parser.add_argument("-r", "--repo", default=REPO)
+parser.add_argument("-r", "--repo")
 
 parser.add_argument("-n", "--storage-block", default="default-gh")
 parser.add_argument("-i", "--image")
