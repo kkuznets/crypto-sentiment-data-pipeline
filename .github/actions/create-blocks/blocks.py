241ccecf9d345a317b6085db0ee3c9196de1b030
Kirill Kuznetsov <kirill@kkuznets.dev>
2023-04-06 12:25:15 +1000

    fix block names

--- .github/actions/create-blocks/blocks.py
+++ .github/actions/create-blocks/blocks.py
@@ -1,6 +1,6 @@
 """
 python blocks.py -b $GITHUB_REF_NAME -r "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" \
--n ${{ inputs.gh_block_name }} -i ${{ inputs.image_uri }} --crj ${{ inputs.crj_block_name }} --region ${{ inputs.region }}
+-n ${{ inputs.storage_block }} -i ${{ inputs.image_uri }} --gcp ${{ inputs.gcp_creds_block }} --crj ${{ inputs.infrastructure_block }} --region ${{ inputs.region }}
 """
 import argparse
 from prefect.filesystems import GitHub
@@ -12,9 +12,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--branch", default="main")
 parser.add_argument("-r", "--repo", default=REPO)
 
-parser.add_argument("-n", "--gh-block-name", default="default-gh")
+parser.add_argument("-n", "--storage-block", default="default-gh")
 parser.add_argument("-i", "--image")
-parser.add_argument("--crj", "--crj-block-name", default="default-crj")
+parser.add_argument("--gcp", "--gcp-creds-block", default="default-gcp")
+parser.add_argument("--crj", "--infrastructure-block", default="default-crj")
 parser.add_argument("--region", default="us-central1")
 
 args = parser.parse_args()
@@ -25,7 +26,7 @@ gh.save(args.gh_block_name, overwrite=True)
 block = CloudRunJob(
     image=args.image,
     region=args.region,
-    credentials=GcpCredentials.load(args.crj_block_name),
+    credentials=GcpCredentials.load(args.gcp_block_name),
     cpu=1,
     timeout=3600,
 )
