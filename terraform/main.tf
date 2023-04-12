0a7dcd9c0d30c5922dac866187fc3e6e8ce70548
Kirill Kuznetsov <kirill@kkuznets.dev>
2023-04-12 11:48:01 +1000

    fix gcs config

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,21 +9,25 @@ terraform {
 provider "google" {
 }
 
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "lalala" # Concatenating DL bucket & Project name for unique naming
   location      = "EU"
-  force_destroy = true
 
+  # Optional, but recommended settings:
   uniform_bucket_level_access = true
 
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
+  versioning {
+    enabled     = true
   }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
   }
+
+  force_destroy = true
 }
