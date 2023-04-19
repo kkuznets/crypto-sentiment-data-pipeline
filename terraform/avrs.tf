6c92e5cce0cfb4ec6547a2c4e392187e461b43f1
Kirill Kuznetsov <kirill@kkuznets.dev>
2023-04-19 12:02:47 +1000

    format workflows

--- terraform/avrs.tf
+++ terraform/avrs.tf
@@ -1,29 +1,29 @@
-variable "bucket_name" {
-    type        = string
-    description = "Name of the bucket to create"
-    default     = "raw-crypto-market-data"
-}
-
-variable "dataset_name" {
-    type        = string
-    description = "Name of the dataset to create"
-    default     = "crypto-market-data"
+variable "artifact_repository_name" {
+  type        = string
+  description = "Artifact repository with Prefect agent images"
+  default     = "prefect-agent-images"
 }
 
 variable "location" {
-    type        = string
-    description = "Location of the bucket and dataset"
-    default     = "US"
+  type        = string
+  description = "Location of the bucket and dataset"
+  default     = "US"
 }
 
 variable "region" {
-    type        = string
-    description = "GCP region"
-    default     = "us-central1"
+  type        = string
+  description = "GCP region"
+  default     = "us-central1"
 }
 
-variable "artifact_repository_name" {
-    type        = string
-    description = "Artifact repository with Prefect agent images"
-    default     = "prefect-agent-images"
+variable "bucket_name" {
+  type        = string
+  description = "Name of the bucket to create"
+  default     = "raw-crypto-market-data"
+}
+
+variable "dataset_name" {
+  type        = string
+  description = "Name of the dataset to create"
+  default     = "crypto-market-data"
 }
